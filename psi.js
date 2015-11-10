b607d1bcd0aa7f74f71e4968d8bd2f34c25ab1e1
DaleC <summer.dchrislene@gmail.com>
2015-11-10 14:55:34 +0800

    Added styling, adding GMaps - not working yet

--- psi.js
+++ psi.js
@@ -8,10 +8,19 @@ function getPSI(){
     }
   }
 
-  var url = "";
+  var url = "http://www.nea.gov.sg/api/WebAPI/?dataset=psi_update&keyref=781CF461BB6606AD28A78E343E0E417601AD1130D0C89BAC";
   xHttpReq.open("GET",url,true);
   xHttpReq.send();
 
+}
+
+function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'), {
+    //1.3000° N, 103.8000° E
+    center: {lat: 1.3000, lng: 103.8000},
+    scrollwheel: false,
+    zoom: 8
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function(){
